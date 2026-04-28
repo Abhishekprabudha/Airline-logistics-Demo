91142cdb61575589bce24c8684dde8222ba755af
Abhishekprabudha <96419916+Abhishekprabudha@users.noreply.github.com>
2026-04-28 21:15:03 +0530

    Update video source links in videos.config.js

--- videos.config.js
+++ videos.config.js
@@ -1,14 +1,14 @@
 // Google Drive source map for production GitHub Pages deployments.
 // Replace each value with your own Drive share links or direct URLs.
 window.DRIVE_VIDEO_SOURCES = {
-  non_ideal_box_flow: '',
-  non_ideal_baggage_flow: '',
-  non_ideal_people_flow: '',
-  box_math_detection: '',
-  baggage_math_detection: '',
-  airline_logistics_system: '',
-  dynamic_pricing: '',
-  happy_box_flow: '',
-  happy_baggage_flow: '',
-  happy_people_flow: ''
+  non_ideal_box_flow: 'https://drive.google.com/file/d/1ZAWF_zrkNTEwzoIKl_CIeAy59_vTbs1R/view?usp=sharing',
+  non_ideal_baggage_flow: 'https://drive.google.com/file/d/1lTQbVPOosWqiie2NJMISCgepbBOzyIWR/view?usp=sharing',
+  non_ideal_people_flow: 'https://drive.google.com/file/d/1lctFKmIDhUeEt7KMOIWC6AnutUthIMZy/view?usp=sharing',
+  box_math_detection: 'https://drive.google.com/file/d/1rcAQGXilACCGS4qQSJ_8EBxFipo8hB1a/view?usp=sharing',
+  baggage_math_detection: 'https://drive.google.com/file/d/12T_56_I9X5eWVv9Ph-UEsGJXPXKCSChO/view?usp=sharing',
+  airline_logistics_system: 'https://drive.google.com/file/d/1vnF3HUqzqx5gMiHne3RQ3bnRXwhh61Q3/view?usp=sharing',
+  dynamic_pricing: 'https://drive.google.com/file/d/1jC7c9wQy8al4okT3q2lR6zJYh0ApXxVm/view?usp=sharing',
+  happy_box_flow: 'https://drive.google.com/file/d/1JArdxvbn88W3x9wdyTNTfYbjE7wSqWaD/view?usp=sharing',
+  happy_baggage_flow: 'https://drive.google.com/file/d/1mR90SiItlgVom3USAaS-DWt2hqBBgCf-/view?usp=sharing',
+  happy_people_flow: 'https://drive.google.com/file/d/1eFqMW3RrKx1zZFRexWnqCMEMm-spGd27/view?usp=sharing'
 };
